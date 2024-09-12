317177af8e5f913afca9813345641e20c68b3eb0
HusseinHalloum <hseinhalloum00@gmail.com>
2024-09-12 12:44:34 +0300

    Remove App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import './App.css'
 import Footer from './components/layout/Footer'
 
 function App() {
